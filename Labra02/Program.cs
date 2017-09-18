fe6a79ffb0980443500268fb06153ad455d920f0
Anzhelika Kettunen <K8993@student.jamk.fi>
2017-09-18 10:40:22 +0300

    T1 added

--- Labra02/Program.cs
+++ Labra02/Program.cs
@@ -10,8 +10,50 @@ namespace Labra02
     {
         static void Main(string[] args)
         {
-            Labra02.test.Test();
-            Console.Read();
+            
+                int tehtava;
+                do
+                {
+                    Console.Write("Valitse tehtävän numero (1 - 7) > ");
+                    tehtava = 99;
+                    tehtava = Convert.ToInt32(Console.ReadLine());
+                    switch (tehtava)
+                    {
+                        case 1:
+                            Labra02.T1.Tehtava();
+                            break;
+                       /* case 2:
+                            Labra02.T2.Tehtava();
+                            break;
+                        case 3:
+                            Labra02.T3.Tehtava();
+                            break;
+                        case 4:
+                            Labra02.T4.Tehtava();
+                            break;
+                        case 5:
+                            Labra01.T5.Tehtava();
+                            break;
+                        case 6:
+                            Labra01.T6.Tehtava();
+                            break;
+                        case 7:
+                            Labra01.T7.Tehtava();
+                            break;
+                       */
+                        default:
+                            Labra02.T1.Tehtava();
+                            break;
+
+                    }
+                    
+
+                } while (tehtava != 0);
+
+
+                Console.Read();
+
+            
         }
     }
 }
--- Labra02/T1.cs
+++ Labra02/T1.cs
@@ -9,8 +9,108 @@ namespace Labra02
     class T1
     {
         public static void Tehtava()
+
         {
-            Console.WriteLine();
+            Console.WriteLine("Tehtävä: ohjelmoida kiukaan toiminta");
+            Kiuas kiuas = Start();
+            Aseta_lampotila(kiuas);
+            Console.Write("Aseta kosteus > ");
+            kiuas.Kosteus = Convert.ToInt32(Console.ReadLine());
+            string vastaus;
+            do
+            {
+                Console.Write("Haluatko laittaa pois päältä? (y/n)");
+                vastaus = Console.ReadLine();
+                if (vastaus == "y")
+                {
+                    kiuas.on_paalla = false;
+                    Console.Write("Kiuas on pois päältä");
+
+                }
+                else Aseta_lampotila(kiuas);
+            } while (vastaus != "y");
+            
+
+            
+
         }
+         
+        public static Kiuas Start()
+        {
+            Console.Write("Aseta ilma lämpötila > ");
+            int ilma_lampotila = Convert.ToInt32(Console.ReadLine());
+            Kiuas kiuas;
+            
+            string vastaus;
+            do
+            {
+                Console.WriteLine("Kiuas on pois päältä. Haluatko laittaa sen päälle? (y/n)");
+                vastaus = Console.ReadLine();
+                if (vastaus == "n")
+                {
+                    Console.WriteLine("Hyvästi!");
+                    Console.ReadLine();
+                    System.Environment.Exit(1);
+
+                } 
+                
+            } while (vastaus != "y");
+
+            kiuas = new Kiuas(ilma_lampotila);
+            
+            return kiuas;
+
+        }
+        
+        static Kiuas Aseta_lampotila(Kiuas kiuas)
+        {
+            Console.Write("Aseta haluamasi lämpötilä > ");
+            int lampo = Convert.ToInt32(Console.ReadLine());
+            if (kiuas.Lampotila < lampo) kiuas.Lampenee(lampo);
+            if (kiuas.Lampotila > lampo) kiuas.Viilentaa(lampo);
+                
+            
+            return kiuas;
+        }
+    }
+    public class Kiuas
+    {
+        public int Lampotila { get; set; }
+        public int Kosteus { get; set; }
+        public bool on_paalla { get; set; }
+        public Kiuas ()
+        {
+            this.on_paalla = true;
+        }
+        public Kiuas (int ilma_lampotila) :base()
+        {
+            this.Lampotila = ilma_lampotila;
+            Console.WriteLine("Nyt kiuas on päällä, sen lämpötila on sama, kuin ilman lämpötila: {0} astetta", ilma_lampotila);
+        }
+        // Methods
+        
+        public void Lampenee(int lampo)
+        {
+            do
+            {
+                this.Lampotila += 1;
+                Console.WriteLine("Kiuan lämpötila nousee ({0})", (this.Lampotila));
+
+            } while (this.Lampotila < lampo);
+            Console.WriteLine("Kiuan lämpötila on ({0})", (this.Lampotila));
+
+
+        }
+        public void Viilentaa(int lampo)
+        {
+            do
+            {
+                this.Lampotila -= 1;
+                Console.WriteLine("Kiuan lämpötila laskee ({0})", (this.Lampotila));
+
+            } while (this.Lampotila > lampo);
+            Console.WriteLine("Kiuan lämpötila on ({0})", (this.Lampotila));
+        }
+        
     }
 }
