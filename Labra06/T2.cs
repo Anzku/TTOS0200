e58461a6f200e7e9767c65921b134b34bb2d64f8
Anzhelika Kettunen <K8993@student.jamk.fi>
2017-10-30 13:24:48 +0200

    T2 added

---
+++ Labra06/T2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra06
+{
+    class T2
+    {
+        public static void Test()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string filename = path + @"\test.txt";
+            StreamReader sr = new StreamReader(filename);
+            if (File.Exists(filename))
+            {
+                string nimi = File.ReadLines(;
+            }
+        }
+    }
+}
